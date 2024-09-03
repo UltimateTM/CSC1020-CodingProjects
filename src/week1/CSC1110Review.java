0ac08029e607d71004218e51c88a327a062b4ac2
Gabriel <gabriellimberg@gmail.com>
2024-09-03 08:34:42 -0500

    setting repository

---
+++ src/week1/CSC1110Review.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class CSC1110Review {
+    public static void main(String[] args) {
+        int b; // declaration (on this line of code Java sets b to zero)
+        b = 6; // Java assigns 6 to b value
+        int a = 5; // initilization
+    }
+}
