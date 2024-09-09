34eeb1b922040aaa8eec96f08e1c6ed45f220e5f
Gabriel <gabriellimberg@gmail.com>
2024-09-09 08:10:17 -0500

    Sending week 1 to repository

---
+++ src/week2/Paths.java
@@ -0,0 +1,8 @@
+package week2;
+
+public class Paths {
+    public static void main(String[] args) {
+        // absolute path (i.e C:\ProgramFiles\Java\HW.java
+        // relative path (HW.java ^^)
+    }
+}
